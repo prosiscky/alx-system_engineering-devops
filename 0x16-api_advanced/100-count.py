deda3cc4aba6d17152ef8fecf852c6608b243bee
prosiscky <legendaryprosperity001@gmail.com>
2023-06-07 05:15:28 +0100

    A script that counts words in all hot posts for a reddit api

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,53 +1,38 @@
 #!/usr/bin/python3
-"""a recursive function that queries the Reddit API, parses
-    the title of all hot articles, and prints a sorted count
-    of given keywords
+"""Function to count words in all hot posts of a given Reddit subreddit
+   that are found in a list
 """
 import requests
 
 
-def count_words(subreddit, word_list, kwargs={}, after=None):
-    """Recursively query the Reddit API, parse the titles of all hot articles,
-    and count the occurrence of given keywords. Print the count in descending
-    order of occurrence, then alphabetically if the count is the same.
+def count_words(subreddit, word_list, array=None, after=""):
+    if array is None:
+        array = {}
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    params = {"after": after}
+    data = requests.get(url, headers=headers, params=params,
+                        allow_redirects=False)
 
-    Args:
-        subreddit (str): The name of the subreddit to query.
-        word_list (List[str]): The list of keywords to count.
-        count_dict (Dict[str, int], optional): The dictionary
-        of keyword counts.
-
-            Defaults to None, which creates an empty dictionary.
-        after (str, optional): The Reddit API parameter to get the
-        next page of results.
-
-            Defaults to None, which gets the first page of results.
-
-    Returns:
-        None
-    """
-    url = "https://www.reddit.com/r/{}/hot/.json?after={}".format(
-                        subreddit, after)
-    headers = {"User-Agent": "custom"}
-
-    data = requests.get(url, headers=headers, allow_redirects=False)
     if data.status_code == 200:
-        after = data.json().get("data").get("after")
-        data = data.json().get("data").get("children")
-        for entry in data:
-            parse_list = entry.get("data").get("title").lower().split()
-            for word in parse_list:
-                for element in word_list:
-                    if word == element:
-                        if element in kwargs:
-                            kwargs[element] += 1
-                        else:
-                            kwargs[element] = 1
+        results = data.json().get("data")
+        after = results.get("after")
+
+        for entry in results.get("children"):
+            split = entry.get("data").get("title").lower().split()
+
+            for word in word_list:
+                if word.lower() in split:
+                    times = len([t for t in split if t == word.lower()])
+                    if array.get(word) is None:
+                        array[word] = times
+                    else:
+                        array[word] += times
+
         if after:
-            count_words(subreddit, word_list, kwargs, after)
+            count_words(subreddit, word_list, array, after)
         else:
-            sorted_dict = {k: v for k, v in sorted(kwargs.items(),
-                           key=lambda item: item[1])}
-            [print("{}: {}".format(k, v)) for k, v in sorted_dict.items()]
-    else:
-        return (None)
+            sorted_results = sorted(array.items(), key=lambda x: (-x[1],
+                                    x[0].lower()))
+            for word, count in sorted_results:
+                print("{}: {}".format(word.lower(), count))
