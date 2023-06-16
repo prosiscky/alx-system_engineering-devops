b42074f298fceff8dac7c5380329fc1f6917323f
prosiscky <legendaryprosperity001@gmail.com>
2023-06-16 02:35:16 +0000

    increase ulimit to 4096

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,12 @@
-# A script that increase the limit of cuncurrent tasks
-file { '/etc/default/nginx':
-  ensure  => file,
-  content => "ulimit -n 4096\n",
+# Fix problem of high amount of requests
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
